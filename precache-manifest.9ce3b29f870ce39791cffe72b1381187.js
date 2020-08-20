fe7b9ea1492e2f8fada5480c7118c0888efc3aa6
Yuvrajhere <yuvrajisbest13@gmail.com>
2020-08-20 17:51:03 +0530

    Updates

---
+++ precache-manifest.9ce3b29f870ce39791cffe72b1381187.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ef8a8588985206753e91ab1ceee2b8",
+    "url": "/github-user-card-react/index.html"
+  },
+  {
+    "revision": "e8aafc7699ac854e9aac",
+    "url": "/github-user-card-react/static/css/main.1a79cc78.chunk.css"
+  },
+  {
+    "revision": "215db7e7b15dc9065fe9",
+    "url": "/github-user-card-react/static/js/2.831d1c18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-user-card-react/static/js/2.831d1c18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8aafc7699ac854e9aac",
+    "url": "/github-user-card-react/static/js/main.1e375b4f.chunk.js"
+  },
+  {
+    "revision": "6e28b96a4f40bd18ee1e",
+    "url": "/github-user-card-react/static/js/runtime-main.fa6b0c16.js"
+  }
+]);
